48eccc40aa64e612ee0ff32d95f364549743b939
Dmitry Conquer <dmitry.conquer@gmail.com>
2024-03-27 17:44:12 +0200

    sort media queries fix

--- gulp/tasks/style.js
+++ gulp/tasks/style.js
@@ -3,32 +3,35 @@ import gulpSass from 'gulp-sass';
 import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
 import cssnano from 'cssnano';
-import combineMediaQueries from 'postcss-combine-media-query';
+import sortMediaQueries from 'postcss-sort-media-queries';
 
 const sass = gulpSass(dartSass);
 
 export const style = () => {
-   const plugins = [
-      autoprefixer({ grid: true, overrideBrowserslist: 'last 2 versions' }),
-      cssnano(),
-      combineMediaQueries(),
-   ];
+  const plugins = [
+    sortMediaQueries({
+      sort: 'desktop-first',
+    }),
 
-   return app.gulp
-      .src(app.path.src.scss, { sourcemaps: app.isDev })
-      .pipe(
-         sass({
-            outputStyle: 'expanded',
-         }),
-      )
+    autoprefixer({ grid: true, overrideBrowserslist: 'last 2 versions' }),
+    cssnano(),
+  ];
 
-      .pipe(app.plugins.if(app.isBuild, postcss(plugins)))
-      .pipe(
-         app.plugins.rename({
-            extname: '.min.css',
-         }),
-      )
-      .pipe(app.plugins.if(app.isBuild, app.plugins.size({ title: 'CSS', showFiles: true })))
-      .pipe(app.gulp.dest(app.path.build.css, { sourcemaps: app.isDev ? '.' : false }))
-      .pipe(app.plugins.browserSync.stream());
+  return app.gulp
+    .src(app.path.src.scss, { sourcemaps: app.isDev })
+    .pipe(
+      sass({
+        outputStyle: 'expanded',
+      }),
+    )
+
+    .pipe(app.plugins.if(app.isBuild, postcss(plugins)))
+    .pipe(
+      app.plugins.rename({
+        extname: '.min.css',
+      }),
+    )
+    .pipe(app.plugins.if(app.isBuild, app.plugins.size({ title: 'CSS', showFiles: true })))
+    .pipe(app.gulp.dest(app.path.build.css, { sourcemaps: app.isDev ? '.' : false }))
+    .pipe(app.plugins.browserSync.stream());
 };
