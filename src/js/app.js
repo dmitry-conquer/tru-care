5943d88d5a6eb2ed97abecc40b2f1b379a0c5d23
Dmitry Conquer <dmitry.conquer@gmail.com>
2024-04-01 17:48:15 +0300

    All pages without refact

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,14 @@
 import sliders from './modules/sliders.js';
 import header from './modules/header.js';
 import fslightbox from 'fslightbox';
-// import { useDynamicAdapt } from './modules/dynamicAdapt.js';
 import AOS from 'aos';
-// import MicroModal from 'micromodal';
+import MicroModal from 'micromodal';
 
 function app() {
+  MicroModal.init({
+    disableScroll: true,
+    disableFocus: true,
+  });
   AOS.init({
     once: true,
   });
