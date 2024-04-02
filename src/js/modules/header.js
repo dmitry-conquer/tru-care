9987c9a1f29f3a71a8c6d9e5dd14b9fd36b24245
Dmitry Conquer <dmitry.conquer@gmail.com>
2024-04-02 10:06:39 +0300

    Refact -> Done

--- src/js/modules/header.js
+++ src/js/modules/header.js
@@ -1,15 +1,21 @@
+import { isMobile } from '../functions.js';
+
 const burgerButton = document.getElementById('burger-button');
 const headerMenu = document.getElementById('header-nav');
 const hasChildrenItems = document.querySelectorAll('.menu-item-has-children');
 hasChildrenItems.forEach(item => {
   item &&
-    item.addEventListener('click', e => {
-      if (window.innerWidth < 1200) {
+    item.addEventListener('click', () => {
+      if (window.innerWidth < 1200 && isMobile.any()) {
         item.classList.toggle('is-open');
         const menu = item.querySelector('ul');
         menu.style.maxHeight = null;
         menu.style.maxHeight = item.classList.contains('is-open') ? `${menu.scrollHeight}px` : null;
-        console.log(menu);
+      } else if (window.innerWidth >= 1200 && isMobile.any()) {
+        hasChildrenItems.forEach(item => {
+          item.classList.remove('is-open-pc');
+        });
+        item.classList.toggle('is-open-pc');
       }
     });
 });
@@ -19,3 +25,11 @@ burgerButton &&
     headerMenu.classList.toggle('is-open');
     document.body.style.overflow = headerMenu.classList.contains('is-open') ? 'hidden' : 'visible';
   });
+
+document.body.addEventListener('click', e => {
+  if (!e.target.closest('.menu-item-has-children')) {
+    hasChildrenItems.forEach(item => {
+      item.classList.remove('is-open-pc');
+    });
+  }
+});
